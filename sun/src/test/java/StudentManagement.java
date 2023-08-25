f4abc906ed33b9bdf3f6a9a7e1a9d2c6dbf29f6d
王敏 <wangmin.wm@shuwen.com>
2023-08-25 09:23:35 +0800

    优化代码日志

--- sun/src/test/java/StudentManagement.java
+++ sun/src/test/java/StudentManagement.java
@@ -11,14 +11,10 @@ import java.util.concurrent.CopyOnWriteArrayList;
 @Slf4j
 public class StudentManagement {
 
-    /**
-     * 防止多线程的操作使用：CopyOnWriteArrayList
-     */
     private static final List<Student> students = new CopyOnWriteArrayList<>();
 
     public static void addStudent(String id, String name, String sex) {
         Student student = Student.builder().name(name).id(id).sex(sex).build();
-        log.info("学号: {}, 姓名: {}, 性别: {}", student.getId(), student.getName(), student.getSex());
         students.add(student);
     }
 
@@ -28,9 +24,15 @@ public class StudentManagement {
     }
 
     public static void main(String[] args) {
-        String[][] info = {{"1001", "张三", "男"}, {"1002", "李四", "女"}, {"1003", "王五", "男"}};
+        String[][] info =
+                {
+                        {"1001", "张三", "男"},
+                        {"1002", "李四", "女"},
+                        {"1003", "王五", "男"}
+                };
         log.info("添加学员信息:");
         Arrays.stream(info).forEach(student -> addStudent(student[0], student[1], student[2]));
+        students.forEach(student -> log.info("学号: {}, 姓名: {}, 性别: {}", student.getId(), student.getName(), student.getSex()));
         deleteStudent("1001");
         log.info("删除后学员信息:");
         students.forEach(student -> log.info("学号: {}, 姓名: {}, 性别: {}", student.getId(), student.getName(), student.getSex()));
